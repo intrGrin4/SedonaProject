dfd5690fe6dcf9339a0c31d7352c6077c42ad91b
intrGrin4 <112719094+intrGrin4@users.noreply.github.com>
2023-09-29 11:20:45 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,1 @@
+const burgerBtn=document.querySelector(".main-header__toggle"),mainHeader=document.querySelector(".main-header");mainHeader.classList.remove("no-js"),burgerBtn.addEventListener("click",(()=>mainHeader.classList.toggle("is-open")));
